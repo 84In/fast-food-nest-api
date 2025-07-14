819f15e703dae0d7419a18bd9a3d0e2256ff4957
84In <trungtin7166293@gmail.com>
2025-07-14 23:14:27 +0700

    feat(config): database connection setup [2]

---
+++ src/config/sequelize.config.ts
@@ -0,0 +1,15 @@
+import { ConfigService } from '@nestjs/config';
+import { SequelizeModuleOptions } from '@nestjs/sequelize';
+import { Dialect } from 'sequelize';
+
+export const sequelizeConfig = (
+  configService: ConfigService,
+): SequelizeModuleOptions => ({
+  dialect: configService.get<Dialect>('DB_DIALECT'),
+  host: configService.get<string>('DB_HOST'),
+  port: configService.get<number>('DB_PORT'),
+  username: configService.get<string>('DB_USERNAME'),
+  password: configService.get<string>('DB_PASSWORD'),
+  database: configService.get<string>('DB_DATABASE'),
+  autoLoadModels: true,
+});
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = new ConfigService();
+  const logger = new Logger('Bootstrap');
+  logger.log(
+    `Server started on port: ${configService.get<number>('PORT') || 3000}`,
+  );
+  const port = configService.get<number>('PORT') || 3000;
+  await app.listen(port);
+  // await app.listen(process.env.PORT ?? 3000);
 }
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
