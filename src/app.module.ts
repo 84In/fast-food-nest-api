7cdad4bfc911b3dbaffe003c6b8bc8f81fdc02a6
84In <trungtin7166293@gmail.com>
2025-07-14 22:58:41 +0700

    feat(config): initial database connection setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './modules/user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule, SequelizeModuleOptions } from '@nestjs/sequelize';
+import { Dialect } from 'sequelize';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    SequelizeModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService): SequelizeModuleOptions => ({
+        dialect: configService.get<Dialect>('DB_DIALECT'),
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        autoLoadModels: true,
+      }),
+    }),
+  ],
   controllers: [],
 })
 export class AppModule {}
